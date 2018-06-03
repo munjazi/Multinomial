3d149bad7b55f5480c7b2577e2f57b65f75db686
munjazi <34534259+munjazi@users.noreply.github.com>
2018-06-03 23:37:01 +0700

    Update CustBehavior.R
    Add clue each prosses

--- ProjectBandung/CustBehavior.R
+++ ProjectBandung/CustBehavior.R
@@ -6,7 +6,10 @@ require(rpart.plot)
 require(RColorBrewer)
 require(randomForest)
 
-mydata=read.csv("D:\\Workspace\\Git\\R-Programming\\ProjectBandung\\advertising.csv",header=TRUE)
+#load dataset
+mydata=read.csv("~.\\advertising.csv",header=TRUE)
+
+#simplify column names
 colnames(mydata)[which(names(mydata) == "Daily.Time.Spent.on.Site")] <- "spent"
 colnames(mydata)[which(names(mydata) == "Daily.Internet.Usage")] <- "usage"
 colnames(mydata)[which(names(mydata) == "Area.Income")] <- "income"
@@ -19,20 +22,25 @@ colnames(mydata)[which(names(mydata) == "Timestamp")] <- "timestamp"
 colnames(mydata)[which(names(mydata) == "Clicked.on.Ad")] <- "clickad"
 names(mydata)
 
+#edit data type
 mydata$age=as.numeric(mydata$age)
 mydata$gender=as.numeric(mydata$gender)
 summary(mydata)
 
+#split data into train and test dataset
 set.seed(123)
 sample = sample.split(mydata,SplitRatio = 0.8)
 train=subset(mydata,sample ==TRUE)
 test=subset(mydata, sample==FALSE)
 
+#Apply the 1st Logistic Regression algorithm
 log_fit1<-glm(clickad~spent+usage+income+age+country+gender,family = "binomial",data = train)
 summary(log_fit1)
+#Apply the 2nd Logistic Regression algorithm
 log_fit2<-glm(clickad~spent+usage+income+age,family = "binomial",data = train)
 summary(log_fit2)
 
+#Tabulation training model prediction and real data
 ptrain <- predict(log_fit1,newdata=train,type="response")
 gg1=floor(ptrain+0.5)
 tab1=table(train$clickad,gg1)
@@ -42,6 +50,7 @@ gg2=floor(ptrain2+0.5)
 tab2=table(train$clickad,gg2)
 tab2
 
+#Tabulation test model prediction and real data
 ptest <- predict(log_fit1,newdata=test,type="response")
 gg3=floor(ptest+0.5)
 tab3=table(test$clickad,gg3)
@@ -51,7 +60,7 @@ gg4=floor(ptest2+0.5)
 tab4=table(test$clickad,gg4)
 tab4
 
-
+#Apply the Desicion Tree Algorithm
 tree1 = rpart(clickad~spent+usage+income+age+country+gender, data=train, method = "class")
 fancyRpartPlot(tree1)
 dttest <- predict(tree1, test, type = "class")
@@ -71,6 +80,7 @@ rftest <- predict(my_forest, test)
 rforest <- data.frame(test, clickad = rftest )
 rforest[1:5,]
 
+#create random forest plot
 varImpPlot(my_forest)
 
 
