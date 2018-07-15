6f2c036ed52ba96766e465a03832ea889f358365
munjazi <munjazzy@gmail.com>
2018-07-15 17:28:21 +0700

    logika

---
+++ iseng.R
@@ -0,0 +1,15 @@
+k=0
+i=0
+while(k==0)
+{
+ if(i%%10!=9){i=i+1}
+ else if(i%%9!=8){i=i+1}
+ else if(i%%8!=7){i=i+1}
+ else if(i%%7!=6){i=i+1}
+ else if(i%%6!=5){i=i+1}
+ else if(i%%5!=4){i=i+1}
+ else if(i%%4!=3){i=i+1}
+ else if(i%%3!=2){i=i+1}
+ else {k=i}
+}
+k
